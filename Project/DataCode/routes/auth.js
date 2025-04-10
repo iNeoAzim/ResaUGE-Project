68ca09d799cfd33622e96778070f16c3cdf40074
Alexouuuu <alexchisiu@gmail.com>
2025-04-10 09:40:26 +0200

    login reload

--- Project/DataCode/routes/auth.js
+++ Project/DataCode/routes/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('./User');
 
 // Middleware pour vérifier si l'utilisateur est administrateur
 const verifyAdmin = async (req, res, next) => {
