da004f4913c7e68dcde6decf3b121d627d647c08
Alexouuuu <alexchisiu@gmail.com>
2025-05-02 21:51:14 +0200

    index.html changes

--- Project/Code/script.js
+++ Project/Code/script.js
@@ -9,3 +9,28 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const registerRoleSelect = document.getElementById('register-role');
+const loginRoleSelect = document.getElementById('login-role');
+const registerRoleDescription = document.getElementById('role-description-register');
+const loginRoleDescription = document.getElementById('role-description-login');
+
+const roleDescription = {
+    student: "texte à afficher pour l'élève",
+    teacher: "texte à afficher pour le professeur",
+    agent: "texte à afficher pour l'agent",
+    admin: "texte à afficher pour l'administrateur",
+};
+
+function updateRoleDescription(selectElement, descriptionElement) {
+    const selectedRole = selectElement.value;
+    descriptionElement.textContent = roleDescription[selectedRole];
+}
+
+registerRoleSelect.addEventListener('change', () => {
+    updateRoleDescription(registerRoleSelect, registerRoleDescription);
+});
+
+loginRoleSelect.addEventListener('change', () => {
+    updateRoleDescription(loginRoleSelect, loginRoleDescription);
+});
