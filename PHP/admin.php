92845c23effbe74002817d224bd56bbeeac5289b
Alexouuuu <alexchisiu@gmail.com>
2025-05-03 22:08:50 +0200

    changement du .html au .php pour les pages

---
+++ PHP/admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réservations administrateurs - Université Eiffel</title>
+    <link rel="stylesheet" href="/Project/Code/style.css">
+</head>
+<body id="page0">
+
+   
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Tableau de bord</a>
+        <a href="#">Utilisateurs</a>
+        <a href="#">Suivi</a>
+        <a href="#">Objets et salles</a>
+    </nav>
+
+    <div class="main-content">
+        <h1>Bonjour ! {username}</h1>
+        <h2>Réserver</h2>
+        <div class="course-cards">
+            <div class="course-card">
+                <img src="." alt="gest0">
+                <h3>Gestion des salles</h3>
+                <p>.</p>
+            </div>
+            <div class="course-card">
+                <img src="." alt="gest1">
+                <h3>Gestion du matériel</h3>
+                <p>.</p>
+            </div>
+            <div class="course-card">
+                <img src="." alt="gest2">
+                <h3>Gestion des utilisateurs</h3>
+                <p>.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <footer>
+        <p>&copy;2025 Université Eiffel. Tous droits réservés.</p>
+    </footer>
+
+</body>
+</html>
---
+++ PHP/agent.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réservations agents - Université Eiffel</title>
+    <link rel="stylesheet" href="/Project/Code/style.css">
+</head>
+<body id="page0">
+
+   
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Notifications</a>
+        <a href="#">Réservation</a>
+        <a href="#">Historique</a>
+    </nav>
+
+    <div class="main-content">
+        <h1>Bonjour ! {username}</h1>
+        <h2>Réserver</h2>
+        <div class="course-cards">
+            <div class="course-card">
+                <img src="." alt="ag0">
+                <h3>Documents confirmées</h3>
+                <p>.</p>
+            </div>
+            <div class="course-card">
+                <img src="." alt="ag1">
+                <h3>Réservations validées</h3>
+                <p>.</p>
+            </div>
+            <div class="course-card">
+                <img src="." alt="ag2">
+                <h3>Registres</h3>
+                <p>.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <footer>
+        <p>&copy;2025 Université Eiffel. Tous droits réservés.</p>
+    </footer>
+
+</body>
+</html>
