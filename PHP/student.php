164413d0b8ffbd059dae904eeeea5edcd286782b
Alexouuuu <alexchisiu@gmail.com>
2025-05-15 16:40:08 +0200

    many changes

--- PHP/student.php
+++ PHP/student.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réservations étudiants - Université Eiffel</title>
-    <link rel="stylesheet" href="CSS/stylestudent.css">
+    <link rel="stylesheet" href="../CSS/stylestudent.css">
 </head>
 <body id="page0">
 
